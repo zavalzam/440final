2b63477744f8f588aa7119a845a09e8fa8fc2d7f
zavalzam <zavalzam@oregonstate.edu>
2020-03-08 11:37:51 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 // TODO: Add your database configuration to this file.
-// NOTE: If you are using source code control (like GitHub), make sure you 
+// NOTE: If you are using source code control (like GitHub), make sure you
 //       do not check in passwords!
 //
 module.exports = {
   host: 'classmysql.engr.oregonstate.edu',
-  user: 'cs340_zavalzam',
-  password: 'TempPass123',
-  database: 'cs340_zavalzam'
+  user: 'cs440_group17',
+  password: 'tgK2XdE8TJWE',
+  database: 'cs440_group17'
 };
