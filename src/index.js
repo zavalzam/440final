b55cf947e0c4785567f197f72f8d25dcc04ecd2b
zavalzam <zavalzam@oregonstate.edu>
2020-03-12 09:18:44 -0700

    games

--- src/index.js
+++ src/index.js
@@ -139,7 +139,7 @@ app.get('/games', connectDb, function(req, res, next) {
 app.get('/games/:GameID', connectDb, function(req, res, next) {
   let GameID = req.params.GameID;
   console.log(GameID);
-  req.db.query('SELECT * FROM VideoGame WHERE GameID = ?', [GameID], function(err, Games) {
+  req.db.query('SELECT * FROM VideoGame, VideoGame_Genre WHERE VideoGame.GameID = ? AND VideoGame_Genre.GameID = VideoGame.GameID', [GameID], function(err, Games) {
     if (err) return next(err);
     if (Games.length === 0) {
       res.render('404');
